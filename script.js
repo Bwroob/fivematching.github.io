8013807a749fc8e322668c05b0cd67a61443f689
Bwroob <osaaki150@gmail.com>
2024-12-02 17:23:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,3 @@ window.onload = function() {
     alert("ページが読み込まれました！楽しんでください！");
 };
 
-// ボタンのクリックイベントを設定
-document.getElementById('clickMeButton').addEventListener('click', function() {
-    alert("ボタンがクリックされました！");
-})
